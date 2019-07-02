de8beb3dcf53a38104a29da300c03699b5274e4b
NandaMochammad <nanda.mocha@gmail.com>
2019-07-02 10:57:23 +0700

    Initial Commit

---
+++ CDTrial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CDTrial
+//
+//  Created by Nanda Mochammad on 02/07/19.
+//  Copyright © 2019 nandamochammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
